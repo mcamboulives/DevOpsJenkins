614dfc35ad4a20772da76b8852d246342c53f8ad
mcamboulives <camboulivesmanon@gmail.com>
2017-10-06 16:44:34 +0200

    DevOps tpmaven

---
+++ src/main/java/fr/ut2j/m1ice/ci/tpmaven/App.java
@@ -0,0 +1,14 @@
+package fr.ut2j.m1ice.ci.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public final class App {
+    /** Create app main.
+    * @param args arguments list.
+    */
+    private static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/fr/ut2j/m1ice/ci/tpmaven/package-info.java
@@ -0,0 +1,3 @@
+/** Info about this package doing something for package-info.java file.
+ */
+package fr.ut2j.m1ice.ci.tpmaven;
